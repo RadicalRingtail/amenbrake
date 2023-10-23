57c83f984d8059444e18d9c6e43f69451ecf7e27
Ringtail <29079415+RadicalRingtail@users.noreply.github.com>
2023-10-23 04:03:57 -0400

    venv setup changes (might delete idk)

--- venv.py
+++
@@ -1,8 +0,0 @@
-import os, venv, subprocess
-
-# automatically sets up a virual environment for development
-
-current_dir = os.path.join(os.getcwd(), '.venv')
-
-venv.create(current_dir, with_pip=True)
-subprocess.run('.venv\Scripts\pip.exe install -r requirements.txt')
