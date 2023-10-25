1a7ab9d5157c859f282350fb750c641a8b18f83a
Ringtail <29079415+RadicalRingtail@users.noreply.github.com>
2023-10-25 04:30:06 -0400

    added debuging

--- src/main.py
+++ src/main.py
@@ -1,11 +1,39 @@
 from shutil import which
 from tkinter import messagebox
+import os, argparse, cProfile, pstats
+from datetime import datetime
+
 from ui import Window
 
+# command line args
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--debug', action='store_true')
+args = parser.parse_args()
+
 if __name__ == '__main__':
-    # checks if theres a valid ffmpeg install present, if so, it initializes the application
+    # initialize application
 
     if which('ffmpeg') is not None:
-        Window()
+
+        if args.debug:
+            if os.path.exists('debug'):
+                pass
+            else:
+                os.mkdir('debug')
+
+            dump_folder = os.path.join('debug', datetime.today().strftime("%d-%m-%y"))
+
+            if os.path.exists(dump_folder):
+                pass
+            else:
+                os.mkdir(dump_folder)
+
+            debug_output = os.path.join(dump_folder, datetime.now().strftime("%d-%m-%y-%H-%M-%S.prof"))
+
+            profiler = cProfile.run('Window()', debug_output)
+            p = pstats.Stats(debug_output).sort_stats('cumtime')
+            p.print_stats(20)
+        else:
+            Window()
     else:
         messagebox.showerror(title='Erorr', message='No ffmpeg installation found!')
